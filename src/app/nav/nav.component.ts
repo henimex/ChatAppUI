e91f89613638e0a3ca1f85974b4bdc5968db95ff
henimex <henimex@gmail.com>
2023-01-16 23:32:16 +0300

    I Hate Middlewares and Authentication

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -22,7 +22,7 @@ export class NavComponent implements OnInit {
     this.authService.login(this.model).subscribe(next => {
       console.log("Logged")
     }, error => {
-      console.log('Error ' , error)
+      console.log('Failed To Login ' , error)
     })
   }
 
